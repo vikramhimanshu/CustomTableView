4d8d4e20f52e29bf74c4c7196e1a5af236d6301f
Himanshu Vikram <vikramhimanshu@gmail.com>
2015-02-10 21:05:03 -0800

    Initial Commit

---
+++ CustomTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTableView
+//
+//  Created by Himanshu on 2/10/15.
+//  Copyright (c) 2015 Himanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTableView
+//
+//  Created by Himanshu on 2/10/15.
+//  Copyright (c) 2015 Himanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
